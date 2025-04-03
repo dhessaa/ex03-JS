4745562bc2f7c91ed0a02928a4f01ce6694e209f
dhessaa <andressasouza017@gmail.com>
2025-04-03 20:23:23 -0300

    Add files via upload

---
+++ ex010/script.js
@@ -0,0 +1,7 @@
+var  numero = Number(prompt("digite um numero"));
+
+if (numero % 2 == 0) {
+    alert(`${numero} = PAR`);
+} else {
+    alert(`${numero} = IMPAR`);
+}
---
+++ ex011/script.js
@@ -0,0 +1,4 @@
+var valortotal = Number(prompt("digite o valor total da comprar"));
+var codigoDesconto = prompt("digite o código de desconto");
+
+if ()
